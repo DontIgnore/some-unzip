6e58e68ee23c0b20125e1cbb805b9d6f8d3be5b3
Ramziddin Makhmudov <ramziddin.makhmudov@gmail.com>
2022-06-17 13:16:16 +0500

    Test

--- build/bundle.js
+++ build/bundle.js
@@ -3321,6 +3321,20 @@
 
   var zipBrowser = Unzip;
 
-  console.log(zipBrowser);
+  const form = document.querySelector("#file-form");
+  const fileInput = document.querySelector("#file-input");
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    const file = fileInput.files[0];
+    if (!file) return;
+    const zip = new zipBrowser(file);
+    zip.getBuffer(["test.html"], {}, (err, buffers) => {
+      if (err) console.error(err);
+      Object.keys(buffers).forEach(key => {
+        const buffer = buffers[key];
+        console.log(key, buffer.toString());
+      });
+    });
+  });
 
 })();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import Unzip from "isomorphic-unzip"
 
-console.log(Unzip)
+const form = document.querySelector("#file-form")
+const fileInput = document.querySelector("#file-input")
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  const file = fileInput.files[0]
+
+  if (!file) return
+
+  const zip = new Unzip(file)
+
+  zip.getBuffer(["test.html"], {}, (err, buffers) => {
+    if (err) console.error(err)
+
+    Object.keys(buffers).forEach((key) => {
+      const buffer = buffers[key]
+      console.log(key, buffer.toString())
+    })
+  })
+})
