cc7e8d81ddd7eddf96487b366a600a8cc44a4f06
Ramziddin Makhmudov <ramziddin.makhmudov@gmail.com>
2022-06-18 21:10:45 +0500

    Fix

--- src/index.js
+++ src/index.js
@@ -1,92 +1,92 @@
-import Unzip from "isomorphic-unzip";
+import Unzip from "isomorphic-unzip"
 
-import * as XLSX from "xlsx/xlsx.mjs";
+import * as XLSX from "xlsx"
 
-import convert from "xml-js";
+import convert from "xml-js"
 
-const form = document.querySelector("#file-form");
-const fileInput = document.querySelector("#file-input");
+const form = document.querySelector("#file-form")
+const fileInput = document.querySelector("#file-input")
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
+  e.preventDefault()
 
-  const file = fileInput.files[0];
+  const file = fileInput.files[0]
 
   const country = {
     sender: "820",
     uz: "860",
-  };
+  }
 
-  const coInn = "303360698";
+  const coInn = "303360698"
 
   /* Date init start*/
 
-  let date = new Date();
-  let dd = date.getDate();
-  if (dd < 10) dd = "0" + dd;
-  var mm = date.getMonth() + 1;
-  if (mm < 10) mm = "0" + mm;
-  var yy = date.getFullYear();
-  const now = `${yy}-${mm}-${dd}`;
+  let date = new Date()
+  let dd = date.getDate()
+  if (dd < 10) dd = "0" + dd
+  var mm = date.getMonth() + 1
+  if (mm < 10) mm = "0" + mm
+  var yy = date.getFullYear()
+  const now = `${yy}-${mm}-${dd}`
 
   /* Date init end */
 
   /* Beauty Passport start */
   const beautyPassport = (str) => {
-    let passportStr = str.trim().replace(/\s/g, "");
-    let passport;
+    let passportStr = str.trim().replace(/\s/g, "")
+    let passport
     if (passportStr.length == 9) {
-      passport = { ser: passportStr.slice(0, 2), num: passportStr.slice(2) };
+      passport = { ser: passportStr.slice(0, 2), num: passportStr.slice(2) }
 
       if (
         passport.ser == "АА" ||
         passport.ser == "AА" ||
         passport.ser == "АA"
       ) {
-        passport.ser = "AA";
+        passport.ser = "AA"
       }
       if (
         passport.ser == "АВ" ||
         passport.ser == "AВ" ||
         passport.ser == "АB"
       ) {
-        passport.ser = "AB";
+        passport.ser = "AB"
       }
       if (
         passport.ser == "АС" ||
         passport.ser == "AС" ||
         passport.ser == "АC"
       ) {
-        passport.ser = "AC";
+        passport.ser = "AC"
       }
     } else {
-      passport = { ser: "Passport length !== 9", num: "Passport length !== 9" };
+      passport = { ser: "Passport length !== 9", num: "Passport length !== 9" }
     }
 
-    return passport;
-  };
+    return passport
+  }
   /* Beauty Passport end */
-  if (!file) return;
+  if (!file) return
 
-  const zip = new Unzip(file);
+  const zip = new Unzip(file)
 
   const declList = {
-    main_data
-  };
+    main_data,
+  }
 
   zip.getEntries((error, entries) => {
-    if (error) return console.error(error);
+    if (error) return console.error(error)
 
-    const fileNames = entries.map((entry) => entry.filename);
+    const fileNames = entries.map((entry) => entry.filename)
 
     zip.getBuffer(fileNames, {}, (error, buffers) => {
-      if (error) return console.error(error);
+      if (error) return console.error(error)
 
       Object.keys(buffers).forEach((key) => {
-        const buffer = buffers[key];
+        const buffer = buffers[key]
 
-        const workbook = XLSX.read(buffer);
-        const mySheet = workbook.Sheets["Sheet1"];
+        const workbook = XLSX.read(buffer)
+        const mySheet = workbook.Sheets["Sheet1"]
         const offsets = {
           declInfo: {
             name: mySheet["A1"].w.trim(),
@@ -110,13 +110,13 @@ form.addEventListener("submit", (e) => {
             row_start: 4,
             total_rows: 20,
           },
-        };
+        }
 
         let getAllInfo = (offset) => {
-          if (!offset) return "Check offset";
+          if (!offset) return "Check offset"
 
-          let ConsignmentItems = [];
-          let itemsLength = 0;
+          let ConsignmentItems = []
+          let itemsLength = 0
 
           for (
             let i = offset.declItems.row_start;
@@ -124,7 +124,7 @@ form.addEventListener("submit", (e) => {
             i++
           ) {
             if (!!mySheet[offset.declItems.row_name + i]) {
-              itemsLength++;
+              itemsLength++
             }
           }
 
@@ -138,7 +138,7 @@ form.addEventListener("submit", (e) => {
               quantity: mySheet[offset.declItems.row_quantity + i[0]],
               cost: mySheet[offset.declItems.row_cost + i[0]],
               weight: (offset.declInfo.weight / itemsLength).toFixed(2),
-            };
+            }
 
             if (
               !!currentItem.name &&
@@ -154,7 +154,7 @@ form.addEventListener("submit", (e) => {
                 netto: currentItem.weight,
                 currency: 840,
                 cost: currentItem.cost.w,
-              });
+              })
             }
           }
 
@@ -186,20 +186,20 @@ form.addEventListener("submit", (e) => {
                 ConsignmentItems,
               },
             ],
-          };
-          declList.main_data.push(currentDecl);
-        };
+          }
+          declList.main_data.push(currentDecl)
+        }
 
-        getAllInfo(offsets);
-      });
+        getAllInfo(offsets)
+      })
       var options = {
         compact: true,
         ignoreComment: true,
         spaces: 2,
         fullTagEmptyElement: true,
-      };
+      }
       // console.log(declList);
-      console.log(convert.js2xml(declList, options));
-    });
-  });
-});
+      console.log(convert.js2xml(declList, options))
+    })
+  })
+})
